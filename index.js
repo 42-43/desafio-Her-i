ae4dffbdf47e32269d551a98dd3d8ef88ea81dbb
Gabriel Gimenez Araujo <141414748+42-43@users.noreply.github.com>
2023-11-23 18:05:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,26 @@
-var nomeHeroi = prompt("Digite o nome do herói:");
+const readline = require('readline');
 
-var xpHeroi = prompt("Digite a quantidade de XP do herói:");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-xpHeroi = parseInt(xpHeroi);
-
-var nivel = determinarNivel(xpHeroi);
-
-var mensagem = `O Herói de nome ${nomeHeroi} está no nível de ${nivel}`;
-
-alert(mensagem);
+rl.question('Digite o nome do herói: ', (nomeHeroi) => {
+  rl.question('Digite a quantidade de XP do herói: ', (xpHeroi) => {
+    xpHeroi = parseInt(xpHeroi);
+    const nivel = determinarNivel(xpHeroi);
+    console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`);
+    rl.close();
+  });
+});
 
 function determinarNivel(xp) {
-    if (xp < 1000) {
-        return "Ferro";
-    } else if (xp <= 2000) {
-        return "Bronze";
-    } else if (xp <= 5000) {
-        return "Prata";
-    } else if (xp <= 7000) {
-        return "Ouro";
-    } else if (xp <= 8000) {
-        return "Platina";
-    } else if (xp <= 9000) {
-        return "Ascendente";
-    } else if (xp <= 10000) {
-        return "Imortal";
-    } else {
-        return "Radiante";
-    }
+  if (xp < 1000) return "Ferro";
+  else if (xp <= 2000) return "Bronze";
+  else if (xp <= 5000) return "Prata";
+  else if (xp <= 7000) return "Ouro";
+  else if (xp <= 8000) return "Platina";
+  else if (xp <= 9000) return "Ascendente";
+  else if (xp <= 10000) return "Imortal";
+  else return "Radiante";
 }
-
